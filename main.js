dafb490d249ae496b9f54ac45e799b3e5e4b7148
AntwuanDixon <evanwchipman@gmail.com>
2020-10-15 11:22:43 +0700

    Init repo for Etch A Sketch from The Odin Project
     Project goals and progress as follows:
    - [x] Select the number of rows from dropdown
    - [x] Click button “generate rows”
    - [x] Button click triggers event -> fetch value from dropdown
    - [x] Generate grid with specified number of rows - one div for each cell
    - [x] mouse over cell triggers event -> get target id of cell (div) which triggered event
    - [x] Color background of cell which triggered event
    - [x] Reset button triggers event -> clear all cell background color

---
+++ main.js
@@ -0,0 +1,58 @@
+const genBtn = document.querySelector('#genBtn');
+const resetBtn = document.getElementById('reset');
+const numRows = document.getElementById('numRows');
+
+genBtn.addEventListener("click", genDivs);
+resetBtn.addEventListener("click", resetGrid);
+
+function genDivs () {
+    removeCells();
+    let container = document.querySelector('#container');
+    for (let i = 1; i <= numRows.value; i++) {
+        var row = document.createElement('div');
+        row.className = "row";
+        //row.innerText = i;
+        container.appendChild(row);
+        
+        for (let j = 1; j <= numRows.value; j++) {
+            var cell = document.createElement('div');
+            cell.className = 'gridsquare';
+            cell.id = 'cell' + i +'.' + j;
+            //cell.innerText = i + '.' + j;
+            row.appendChild(cell);
+        }
+    }
+    /*
+    let cellWidth = (100 / numRows.value);
+    row.style.width = cellWidth + "%";
+    console.log(row.style.width);
+    */
+    container.addEventListener('mouseover', fillCell);
+}
+
+function fillCell (e) {
+    const targetCell = document.getElementById(e.target.id);
+
+    if (targetCell !== null && e.target.className == "gridsquare") {
+        targetCell.style.backgroundColor = "blue";
+    }
+    return targetCell;
+    }
+
+function resetGrid() {
+    const allCells = document.getElementsByClassName("gridsquare");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = "white";
+    }  
+}
+
+function removeCells() {
+    let gridRows = document.getElementsByClassName('row');
+    let gridCells = document.getElementsByClassName('gridsquare');
+    while (gridRows.length > 0) {
+        while (gridCells.length > 0) {
+            gridCells[0].parentNode.removeChild(gridCells[0]); 
+        }
+        gridRows[0].parentNode.removeChild(gridRows[0]);
+    }
+}
