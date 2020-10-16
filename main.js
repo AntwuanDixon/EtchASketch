7b4f19c088502c19350d12a6cd695d1e2d0f95a1
AntwuanDixon <evanwchipman@gmail.com>
2020-10-16 11:35:11 +0700

    complete clickMode, add colors, fonts

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const genBtn = document.querySelector('#genBtn');
 const resetBtn = document.getElementById('reset');
 const numRows = document.getElementById('numRows');
+const clickMode = document.getElementById('clickMode');
+let penColor = document.getElementById('penColor');
+console.log(clickMode.checked)
 genDivs();
 genBtn.addEventListener("click", genDivs);
 resetBtn.addEventListener("click", resetGrid);
+clickMode.addEventListener("click", toggleClickMode)
 
-function genDivs () {
+function genDivs() {
     removeCells();
     let container = document.querySelector('#container');
     for (let i = 1; i <= numRows.value; i++) {
-        var row = document.createElement('div');
+        let row = document.createElement('div');
         row.className = "row";
         //row.innerText = i;
         container.appendChild(row);
         
         for (let j = 1; j <= numRows.value; j++) {
-            var cell = document.createElement('div');
+            let cell = document.createElement('div');
             cell.className = 'gridsquare';
             cell.id = 'cell' + i +'.' + j;
             //cell.innerText = i + '.' + j;
@@ -26,7 +30,17 @@ function genDivs () {
     container.addEventListener('mouseover', fillCell);
 }
 
-function setCellWidth () {
+function toggleClickMode() {
+    if (clickMode.checked === true) {
+        container.removeEventListener('mouseover', fillCell);
+        container.addEventListener('mousedown', fillCell);
+    } else {
+        container.removeEventListener('mousedown', fillCell);
+        container.addEventListener('mouseover', fillCell);
+    }
+}
+
+function setCellWidth() {
     const allRows = document.getElementsByClassName("row");
     let cellWidth = (100 / numRows.value);
     for (let i = 0; i < allRows.length; i++) {
@@ -34,11 +48,14 @@ function setCellWidth () {
     }
 }
 
-function fillCell (e) {
+function fillCell(e) {
     const targetCell = document.getElementById(e.target.id);
-
     if (targetCell !== null && e.target.className == "gridsquare") {
-        targetCell.style.backgroundColor = "blue";
+        if (penColor.value === "Rainbow") {
+            setRandomColor(targetCell);
+        } else {
+            targetCell.style.backgroundColor = penColor.value;
+        }
     }
     return targetCell;
     }
@@ -59,4 +76,17 @@ function removeCells() {
         }
         gridRows[0].parentNode.removeChild(gridRows[0]);
     }
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor(targetCell) {
+    targetCell.style.backgroundColor = getRandomColor();
 }
